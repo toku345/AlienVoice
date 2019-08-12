34152fea9b535e4744e263dd7ef482dc4ee544f4
Fumitaka Tokumitsu <toku345@gmail.com>
2019-08-12 17:28:16 +0900

    Alien Voice? test

--- app/src/main/java/com/toku345/alienvoice/MainActivity.kt
+++ app/src/main/java/com/toku345/alienvoice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.toku345.alienvoice
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mediaPlayer = MediaPlayer.create(applicationContext, R.raw.sample_voice)
+        var playBackParams = mediaPlayer?.playbackParams
+        playBackParams?.pitch = 2.0f
+        mediaPlayer?.playbackParams = playBackParams!!
+        mediaPlayer.start()
     }
 }
