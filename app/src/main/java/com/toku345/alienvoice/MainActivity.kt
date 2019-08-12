21def7f8c34678820723e029b2486d011e0544d0
Fumitaka Tokumitsu <toku345@gmail.com>
2019-08-12 22:49:58 +0900

    Add Start button

--- app/src/main/java/com/toku345/alienvoice/MainActivity.kt
+++ app/src/main/java/com/toku345/alienvoice/MainActivity.kt
@@ -3,6 +3,8 @@ package com.toku345.alienvoice
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(listener)
+    }
+
+    private val listener: View.OnClickListener = View.OnClickListener {
         var mediaPlayer = MediaPlayer.create(applicationContext, R.raw.sample_voice)
         var playBackParams = mediaPlayer?.playbackParams
         playBackParams?.pitch = 2.0f
